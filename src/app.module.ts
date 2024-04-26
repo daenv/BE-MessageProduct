359324b15a61338fb684ae3de7ab880cd70d8500
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-26 14:16:05 +0700

    Update app.module.ts to use @nestjs/config for global configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().port().default(3000),
+      }),
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true,
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
