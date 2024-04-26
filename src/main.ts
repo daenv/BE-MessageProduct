de0c63ec09547bcf6ee862c1a0462efb7dc4fcfa
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-26 08:18:36 +0700

    Update main.ts to use global pipes, enable CORS, and add Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+
+  app.enableCors();
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('API Products')
+    .setDescription('API Products description')
+    .setVersion('1.0')
+    .addTag('products')
+    .build();
+  const document = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('api', app, document);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<string>('PORT') || 3000;
+  await app.listen(port, () => {
+    console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+  });
 }
 bootstrap();
