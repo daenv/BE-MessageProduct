731eba6ffb0d792cdd5b3c4ec4e961b265240849
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-27 22:04:45 +0700

    Add UserEntity class for user properties

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  Entity,
+  Index,
+  JoinTable,
+  ManyToMany,
+  OneToMany,
+} from 'typeorm';
+import { BaseEntity } from './bases';
+import { IUserEntity } from './interfaces';
+import { SessionEntity } from './session.entity';
+import { RoleEntity } from './role.entity';
+
+@Entity({ name: 'users' })
+export class UserEntity extends BaseEntity implements IUserEntity {
+  @Index('IX_USER_USERNAME', { unique: true })
+  @Column({ type: 'varchar', name: 'username' })
+  username: string;
+  @Index('IX_USER_EMAIL', { unique: true })
+  @Column({ type: 'varchar', name: 'email' })
+  email: string;
+  @Column({ type: 'varchar', name: 'password' })
+  password: string;
+  @OneToMany(() => SessionEntity, (session) => session.user)
+  sessions: SessionEntity[];
+
+  @ManyToMany(() => RoleEntity)
+  @JoinTable()
+  roles: RoleEntity[];
+}
