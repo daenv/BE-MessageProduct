f6d43d893a00e1761833034eafadf8d3809025b1
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-27 23:05:45 +0700

    Add entity provider file with user, session, and role entities

---
+++ src/entities/entity.provider.ts
@@ -0,0 +1,5 @@
+import { RoleEntity } from './role.entity';
+import { SessionEntity } from './session.entity';
+import { UserEntity } from './user.entity';
+
+export const entities = [UserEntity, SessionEntity, RoleEntity];
