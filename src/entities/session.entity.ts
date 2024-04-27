8ea484278676df432b4af053287b6677abc90756
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-27 22:04:41 +0700

    Add SessionEntity class for session properties

---
+++ src/entities/session.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { BaseEntity } from './bases';
+import { ISessionEntity } from './interfaces';
+import { UserEntity } from './user.entity';
+
+@Entity({ name: 'sessions' })
+export class SessionEntity extends BaseEntity implements ISessionEntity {
+  @Column({ type: 'varchar', name: 'token' })
+  token: string;
+  @Column({ type: 'timestamptz', name: 'expires_at' })
+  expiresAt: Date;
+  @ManyToOne(() => UserEntity, (user) => user.sessions)
+  user: UserEntity;
+}
