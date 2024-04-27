c98ebfb59a7ed7aad89db8c94eb9a6c30500d7e2
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-27 22:04:50 +0700

    Add IdEntity class for entity identification properties

---
+++ src/entities/bases/id.base.entity.ts
@@ -0,0 +1,11 @@
+import { CreateDateColumn, PrimaryGeneratedColumn } from 'typeorm';
+import { IIdEntity } from '../interfaces/id.interface';
+
+export class IdEntity implements IIdEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @CreateDateColumn({ type: 'timestamptz' })
+  createDateTime: Date;
+  @CreateDateColumn({ type: 'timestamptz' })
+  lastChangedDateTime: Date;
+}
