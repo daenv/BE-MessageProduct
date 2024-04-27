f00b3124f9b8bc7603f871249987263d52500e57
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-27 23:36:51 +0700

    Refactor register method in auth.service.ts

--- src/apps/auth/auth.service.ts
+++ src/apps/auth/auth.service.ts
@@ -1,12 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dtos';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/entities';
+import { UserRepository } from 'src/repositories';
+import { MessageResponse } from 'src/common/interfaces';
 
 @Injectable()
 export class AuthService {
+  /**
+   *
+   */
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly _userRepository: UserRepository,
+  ) {}
   public async login() {}
-  public async register(_req: CreateUserDto, header: any): Promise<unknown> {
-    console.log(header);
-    console.log(_req);
-    return;
+  public async register(
+    _req: CreateUserDto,
+    header: any,
+  ): Promise<MessageResponse> {
+    console.log('header', header);
+
+    return {
+      success: true,
+      message: 'Account created successfully',
+      data: _req,
+    };
   }
 }
