3b65fcbda2be3ab0fb9ee929e677849221fe7811
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-29 15:37:43 +0700

    Refactor AuthService register method to handle username and email validation

--- src/apps/auth/auth.service.ts
+++ src/apps/auth/auth.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dtos';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
+
+import { CreateUserDto } from './dtos';
 import { UserEntity } from 'src/entities';
 import { UserRepository } from 'src/repositories';
 import { MessageResponse } from 'src/common/interfaces';
+import { CustomException } from 'src/common';
 
 @Injectable()
 export class AuthService {
@@ -14,17 +17,74 @@ export class AuthService {
     @InjectRepository(UserEntity)
     private readonly _userRepository: UserRepository,
   ) {}
-  public async login() {}
-  public async register(
-    _req: CreateUserDto,
+
+  /* The `foundUserByUserName` method is a private asynchronous function in the `AuthService` class. It
+  takes a `username` parameter as input and returns a `Promise` that resolves to a `UserEntity`
+  object. */
+  private async foundUserByUserName(username: string): Promise<UserEntity> {
+    const foundUser = await this._userRepository.findOne({
+      where: { username: username },
+    });
+    return foundUser;
+  }
+  private async findUserByEmail(email: string): Promise<UserEntity> {
+    const foundUser = await this._userRepository.findOne({
+      where: {
+        email: email,
+      },
+    });
+    return foundUser;
+  }
+  /**
+   *
+   *
+   */
+
+  private async createUsers(
+    user: CreateUserDto,
     header: any,
   ): Promise<MessageResponse> {
-    console.log('header', header);
+    try {
+      // salt
+      const salt = bcrypt.genSaltSync(10);
+      // hash pw
+      const HashPw = bcrypt.hashSync(user.password, salt);
+      // create user
+      const newUser = await this._userRepository.create({
+        ...user,
+        password: HashPw,
+      });
+      // Generate RSA key pair asynchronously
+      // const  {publicKey, privateKey } = await
+      return {
+        success: false,
+        data: {},
+        message: '',
+      };
+    } catch (error) {
+      throw new CustomException(error.message);
+    }
+  }
 
-    return {
-      success: true,
-      message: 'Account created successfully',
-      data: _req,
-    };
+  public async login() {}
+
+  public async register(_req: CreateUserDto, header: any): Promise<unknown> {
+    // const check username
+    const checkUserName = await this.foundUserByUserName(_req.username);
+    if (checkUserName) {
+      return {
+        success: false,
+        message: 'Username is exist',
+      };
+    }
+    // const check email
+    const checkEmail = await this.findUserByEmail(_req.email);
+    if (checkEmail) {
+      return {
+        success: false,
+        message: 'Email is exist',
+      };
+    }
+    return await this.createUsers(_req, header);
   }
 }
