04083cf34c30eabe44689659d37c972af2cc4327
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-04-29 15:37:08 +0700

    Add KeyTokenEntity to entities in entity.provider.ts

--- src/entities/entity.provider.ts
+++ src/entities/entity.provider.ts
@@ -1,5 +1,6 @@
+import { KeyTokenEntity } from './keyToken.entity';
 import { RoleEntity } from './role.entity';
 import { SessionEntity } from './session.entity';
 import { UserEntity } from './user.entity';
 
-export const entities = [UserEntity, SessionEntity, RoleEntity];
+export const entities = [UserEntity, SessionEntity, RoleEntity, KeyTokenEntity];
