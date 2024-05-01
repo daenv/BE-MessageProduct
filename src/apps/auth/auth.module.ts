f4882b8aaeb9938b0a2f395582b565b789759582
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-05-01 16:55:11 +0700

    Add KeytokenModule and UsersModule imports to AuthModule

--- src/apps/auth/auth.module.ts
+++ src/apps/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { entities } from 'src/entities';
+import { KeytokenModule } from '../keytoken/keytoken.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   controllers: [AuthController],
-  imports: [TypeOrmModule.forFeature(entities)],
+  imports: [TypeOrmModule.forFeature(entities), KeytokenModule, UsersModule],
   providers: [AuthService],
 })
 export class AuthModule {}
