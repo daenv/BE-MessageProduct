e55735b68a7ffce20c0332d2c7956afe5b0483f4
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-05-02 20:45:18 +0700

    Refactor login method in AuthController to accept LoginDto

--- src/apps/auth/auth.controller.ts
+++ src/apps/auth/auth.controller.ts
@@ -1,25 +1,22 @@
-import { Body, Controller, Headers, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import _ from 'underscore';
-import { CreateUserDto } from './dtos';
+import { CreateUserDto, LoginDto } from './dtos';
 import { MessageResponse } from 'src/common/interfaces';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @Post('login')
-  public async login(): Promise<unknown> {
-    const login = await this.authService.login();
+  public async login(@Body() loginDto: LoginDto): Promise<unknown> {
+    const login = await this.authService.login(loginDto);
     return _.omit(login, 'password');
   }
   public async logout(): Promise<unknown> {
     return;
   }
   @Post('register')
-  public async register(
-    @Body() _req: CreateUserDto,
-    @Headers() _headers: any,
-  ): Promise<MessageResponse> {
-    const register = await this.authService.register(_req, _headers);
+  public async register(@Body() _req: CreateUserDto): Promise<MessageResponse> {
+    const register = await this.authService.register(_req);
     return _.omit(register, 'password');
   }
 }
