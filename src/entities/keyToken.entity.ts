29fbfa41c1df0509c7ca7ab067071b6310d4eaba
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-05-06 10:35:54 +0700

    refactor: Remove OneToOne relationship with UserEntity in KeyTokenEntity

--- src/entities/keyToken.entity.ts
+++ src/entities/keyToken.entity.ts
@@ -1,13 +1,10 @@
-import { Column, Entity, OneToOne } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 import { BaseEntity } from './bases';
 import { IKeyTokenEntity } from './interfaces';
-import { UserEntity } from './user.entity';
 @Entity({ name: 'key_tokens' })
 export class KeyTokenEntity extends BaseEntity implements IKeyTokenEntity {
   @Column({ type: 'varchar', name: 'publicKey' })
   publicKey: string;
   @Column({ name: 'refresh_token', type: 'varchar' })
   refreshToken: string[] = [];
-  @OneToOne(() => UserEntity)
-  users: UserEntity[];
 }
