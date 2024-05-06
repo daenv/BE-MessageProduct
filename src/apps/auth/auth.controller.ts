b5911c2d9f1c7d32bbf8f7f921ce09e65bc2e3a4
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-05-06 10:35:37 +0700

    refactor: Update auth controller to remove unnecessary code

--- src/apps/auth/auth.controller.ts
+++ src/apps/auth/auth.controller.ts
@@ -1,22 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import _ from 'underscore';
-import { CreateUserDto, LoginDto } from './dtos';
-import { MessageResponse } from 'src/common/interfaces';
+import { Controller } from '@nestjs/common';
+
 @Controller('auth')
-export class AuthController {
-  constructor(private readonly authService: AuthService) {}
-  @Post('login')
-  public async login(@Body() loginDto: LoginDto): Promise<unknown> {
-    const login = await this.authService.login(loginDto);
-    return _.omit(login, 'password');
-  }
-  public async logout(): Promise<unknown> {
-    return;
-  }
-  @Post('register')
-  public async register(@Body() _req: CreateUserDto): Promise<MessageResponse> {
-    const register = await this.authService.register(_req);
-    return _.omit(register, 'password');
-  }
-}
+export class AuthController {}
