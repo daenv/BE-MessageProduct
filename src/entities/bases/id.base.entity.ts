a7fc4ddea30126f885cd0bdcb739439c5fe269bb
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-05-07 13:58:59 +0700

    refactor: Update IdEntity to include additional fields and constructor

--- src/entities/bases/id.base.entity.ts
+++ src/entities/bases/id.base.entity.ts
@@ -1,11 +1,20 @@
-import { CreateDateColumn, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, CreateDateColumn, PrimaryGeneratedColumn } from 'typeorm';
 import { IIdEntity } from '../interfaces/id.interface';
 
-export class IdEntity implements IIdEntity {
+export class IdEntity<T> implements IIdEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
   @CreateDateColumn({ type: 'timestamptz' })
   createDateTime: Date;
   @CreateDateColumn({ type: 'timestamptz' })
   lastChangedDateTime: Date;
+  @Column({ type: 'boolean', default: false })
+  isActive: boolean;
+  @Column({ type: 'boolean', default: false })
+  isArchived: boolean;
+  @Column({ type: 'varchar', default: '' })
+  createBy: string;
+  constructor(entity: Partial<T>) {
+    Object.assign(this, entity);
+  }
 }
