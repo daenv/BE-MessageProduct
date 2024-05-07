b9ce0fd7b52dcf7d158cb509f5b65976c03db352
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-05-07 12:50:51 +0700

    refactor: Add login and register endpoints to AuthController

--- src/apps/auth/auth.controller.ts
+++ src/apps/auth/auth.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto, LoginDto } from './dtos';
+import _ from 'underscore';
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  /**
+   *
+   */
+  constructor(private readonly _authService: AuthService) {}
+
+  @Post('login')
+  public login(@Body() loginDto: LoginDto) {
+    console.log(loginDto);
+  }
+  @Post('register')
+  public async register(@Body() registerDto: CreateUserDto): Promise<unknown> {
+    const register = await this._authService.register(registerDto);
+    return _.omit(register, 'password');
+  }
+}
