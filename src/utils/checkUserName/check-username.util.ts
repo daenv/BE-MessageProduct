9f05e13231cf6e088787fe7efffe03aa26268aaf
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-05-07 12:51:26 +0700

    refactor: Add checkUsername utility function to validate phone numbers

---
+++ src/utils/checkUserName/check-username.util.ts
@@ -0,0 +1,21 @@
+import { CustomException } from 'src/common';
+
+const containsSpecialCharacters = (str) => {
+  const regex = /[!@#$%^&*(),.?":{}|<>]/;
+  return regex.test(str);
+};
+
+const checkUsername = (phoneNumber) => {
+  try {
+    if (containsSpecialCharacters(phoneNumber)) {
+      throw new CustomException(
+        'Phone number cannot contain special characters',
+      );
+    }
+    return true;
+  } catch (error) {
+    throw new CustomException(error.message);
+  }
+};
+
+export default checkUsername;
