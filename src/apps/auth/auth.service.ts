44280908d2ff7fd09ac206a9761de21f5f87a68f
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-05-07 12:50:55 +0700

    refactor: Add check for username during registration in AuthService

--- src/apps/auth/auth.service.ts
+++ src/apps/auth/auth.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@nestjs/common';
-// import * as bcrypt from 'bcrypt';
-// import { CreateUserDto, LoginDto } from './dtos';
-// import { CustomException, MessageResponse } from 'src/common';
-// import { UsersService } from '../users/users.service';
-// import { KeytokenService } from '../keytoken/keytoken.service';
+import { BadGatewayException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos';
+
+import { UsersService } from '../users/users.service';
+import checkUsername from 'src/utils/checkUserName/check-username.util';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  /**
+   *
+   */
+  constructor(private readonly _userService: UsersService) {}
+
+  public async register(registerDto: CreateUserDto): Promise<unknown> {
+    try {
+      // check UserName
+      checkUsername(registerDto.username);
+
+      const createUser = await this._userService.createUser(registerDto);
+      return createUser;
+    } catch (error) {
+      throw new BadGatewayException(error.message, error.stack);
+    }
+  }
+}
