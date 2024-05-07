f785a3cc7dd301699c2a6d0b10b81e78629b340a
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-05-07 13:58:40 +0700

    refactor: Update KeyTokenEntity to extend IdEntity in keyToken.entity.ts

--- src/entities/keyToken.entity.ts
+++ src/entities/keyToken.entity.ts
@@ -1,8 +1,12 @@
 import { Column, Entity } from 'typeorm';
-import { BaseEntity } from './bases';
+
 import { IKeyTokenEntity } from './interfaces';
+import { IdEntity } from './bases/id.base.entity';
 @Entity({ name: 'key_tokens' })
-export class KeyTokenEntity extends BaseEntity implements IKeyTokenEntity {
+export class KeyTokenEntity
+  extends IdEntity<KeyTokenEntity>
+  implements IKeyTokenEntity
+{
   @Column({ type: 'varchar', name: 'publicKey' })
   publicKey: string;
   @Column({ name: 'refresh_token', type: 'varchar' })
