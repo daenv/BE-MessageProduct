e56907c1a5a1dabd8b8e223ed702b554a37b0204
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-05-08 23:17:53 +0700

    refactor: Update login method in AuthController to return refreshToken

--- src/apps/auth/auth.controller.ts
+++ src/apps/auth/auth.controller.ts
@@ -1,7 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Put, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginDto } from './dtos';
 import _ from 'underscore';
+import { RefreshTokenDto } from './dtos/refreshToken.dto';
+// import { LocalAuthGuard } from './guards/local-auth.guard';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -10,14 +13,23 @@ export class AuthController {
    */
   constructor(private readonly _authService: AuthService) {}
 
+  // @UseGuards(LocalAuthGuard)
   @Post('login')
-  public async login(@Body() loginDto: LoginDto) {
-    const login = await this._authService.login(loginDto);
+  public async login(
+    @Body() loginDto: LoginDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const login = await this._authService.login(loginDto, res);
     return _.omit(login, 'password');
   }
   @Post('register')
   public async register(@Body() registerDto: CreateUserDto): Promise<unknown> {
     const register = await this._authService.register(registerDto);
     return _.omit(register, 'password');
+  }
+  @Put('refresh-token')
+  public async refreshToken(@Body() refreshTokenDto: RefreshTokenDto) {
+    const refresh = await this._authService.refreshToken(refreshTokenDto);
+    return _.omit(refresh, 'password');
   }
 }
