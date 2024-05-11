39562fa836263934e6f0034885f4e18893ac770a
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-05-11 13:02:30 +0700

    feat: Add TypeOrmModule to SessionsModule for managing sessions

--- src/apps/sessions/sessions.module.ts
+++ src/apps/sessions/sessions.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SessionsService } from './sessions.service';
 import { SessionsController } from './sessions.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { entities } from 'src/entities';
 
 @Module({
+  imports: [TypeOrmModule.forFeature(entities)],
   providers: [SessionsService],
-  controllers: [SessionsController]
+  controllers: [SessionsController],
+  exports: [SessionsService],
 })
 export class SessionsModule {}
