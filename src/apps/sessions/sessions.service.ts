119849f854859614e0f31786824a5436bad85011
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-05-11 13:02:35 +0700

    feat: Add findSession method to SessionsService for retrieving session by token

--- src/apps/sessions/sessions.service.ts
+++ src/apps/sessions/sessions.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CustomException } from 'src/common';
+import { SessionEntity } from 'src/entities';
+import { SessionRepository } from 'src/repositories';
 
 @Injectable()
-export class SessionsService {}
+export class SessionsService {
+  /**
+   *
+   */
+  constructor(
+    @InjectRepository(SessionEntity)
+    private readonly _sessionRepository: SessionRepository,
+  ) {}
+
+  public async findSession(token: string): Promise<SessionEntity> {
+    try {
+      const session = await this._sessionRepository.findOne({
+        where: {
+          token: token,
+        },
+        relations: ['users'],
+      });
+      console.log('session::', session);
+      return session;
+    } catch (error) {
+      throw new CustomException(error);
+    }
+  }
+}
