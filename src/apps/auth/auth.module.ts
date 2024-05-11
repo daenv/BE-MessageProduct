14f253e4977f350e63ab445a108d30a5b1c76080
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-05-11 13:02:19 +0700

    feat: Add SessionsModule and SessionsController for managing sessions

--- src/apps/auth/auth.module.ts
+++ src/apps/auth/auth.module.ts
@@ -5,10 +5,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { entities } from 'src/entities';
 import { KeytokenModule } from '../keytoken/keytoken.module';
 import { UsersModule } from '../users/users.module';
+import { SessionsModule } from '../sessions/sessions.module';
 
 @Module({
   controllers: [AuthController],
-  imports: [TypeOrmModule.forFeature(entities), KeytokenModule, UsersModule],
+  imports: [
+    TypeOrmModule.forFeature(entities),
+    KeytokenModule,
+    UsersModule,
+    SessionsModule,
+  ],
   providers: [AuthService],
   exports: [AuthService],
 })
