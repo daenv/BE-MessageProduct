df59565a500cf6be7e86507ca301d368e26f50e4
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-05-15 10:42:44 +0700

    refactor: Add ApiProperty decorators to CreateUserDto properties

--- src/apps/auth/dtos/create-account.dto.ts
+++ src/apps/auth/dtos/create-account.dto.ts
@@ -1,12 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   username: string;
+
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   email: string;
+
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   password: string;
