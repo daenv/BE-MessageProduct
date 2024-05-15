eba5b23c06d2abf6112bb8a206b9acb9207f47a6
daenvngucii <120397126+daenv@users.noreply.github.com>
2024-05-15 10:43:13 +0700

    refactor: Update UserRepository import in user.repository.ts

--- src/repositories/postgres/user.repository.ts
+++ src/repositories/postgres/user.repository.ts
@@ -1,4 +1,4 @@
-import { UserEntity } from 'src/entities';
+import { UserEntity } from 'src/entities/user.entity';
 import { BaseRepository } from './base/base.repository';
 
 export class UserRepository extends BaseRepository<UserEntity> {}
